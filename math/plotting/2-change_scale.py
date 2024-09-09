8eff7e95b9de8219d9f5e0ce390c298e0fdac3a5
Eunice Adewusi <e.adewusi@alustudent.com>
2024-09-09 23:59:30 +0200

    added pycodestyle library

--- math/plotting/2-change_scale.py
+++ math/plotting/2-change_scale.py
@@ -8,15 +8,18 @@ x = np.arange(0, 28651, 5730)
 # calculates the decay constant for carbon-14
 r = np.log(0.5)
 
-t = 5730 # half-life of carbon-14
+t = 5730  # half-life of carbon-14
 
-# calculate the fraction remaining of carbon-14 at each time point using the exponential decay formula: y = e^(rt), where r is the decay constant, t is the time, and y is the fraction remaining
+# calculate the fraction remaining of carbon-14 at each time point using
+# the exponential decay formula: y = e^(rt), where r is the decay
+# constant, t is the time, and y is the fraction remaining
 y = np.exp((r / t) * x)
 
 plt.plot(x, y)
 plt.xlabel('Time (years)')
 plt.ylabel('Fraction Remaining')
 plt.title('Exponential Decay of C-14')
-plt.yscale('log') # sets y-axis to logarithmic scale appropriate for exponential decay
+# sets y-axis to logarithmic scale appropriate for exponential decay
+plt.yscale('log')
 plt.xlim(0, 28650)
 plt.show()
