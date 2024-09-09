8eb6929d829e44a7418b077679eea5ac91b4f0ff
Eunice Adewusi <e.adewusi@alustudent.com>
2024-09-09 23:57:39 +0200

    2. Change of scale

--- math/plotting/1-scatter.py
+++ math/plotting/1-scatter.py
@@ -3,15 +3,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # mean of the distribution - height ininches, weight in pounds
-mean = [69, 0] 
+mean = [69, 0]
 
-# covariance matrix of the distribution, shows relationship between height and weight
-cov = [[15, 8], [8, 15]] 
+# covariance matrix of the distribution, shows relationship between height
+# and weight
+cov = [[15, 8], [8, 15]]
 
-# sets random seed to 5, ensuring that the same random numbers are generated each time the code is run
-np.random.seed(5) 
+# sets random seed to 5, ensuring that the same random numbers are
+# generated each time the code is run
+np.random.seed(5)
 
-# generates 2000 random samples from the distribution with the specified mean and covariance. T transposes (swaps rows and columns) the result, so x contains the height values and y contains the weight values
+# generates 2000 random samples from the distribution with the specified
+# mean and covariance. T transposes (swaps rows and columns) the result,
+# so x contains the height values and y contains the weight values
 x, y = np.random.multivariate_normal(mean, cov, 2000).T
 
 # shifts the weight values upward by 180 pounds
---
+++ math/plotting/2-change_scale.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+# creates a NumPy array x containing values from 0 to 28650 in steps of 5730
+x = np.arange(0, 28651, 5730)
+
+# calculates the decay constant for carbon-14
+r = np.log(0.5)
+
+t = 5730 # half-life of carbon-14
+
+# calculate the fraction remaining of carbon-14 at each time point using the exponential decay formula: y = e^(rt), where r is the decay constant, t is the time, and y is the fraction remaining
+y = np.exp((r / t) * x)
+
+plt.plot(x, y)
+plt.xlabel('Time (years)')
+plt.ylabel('Fraction Remaining')
+plt.title('Exponential Decay of C-14')
+plt.yscale('log') # sets y-axis to logarithmic scale appropriate for exponential decay
+plt.xlim(0, 28650)
+plt.show()
