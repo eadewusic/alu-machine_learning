2328bba1922d6a2022bdbfb2342e0cead1441409
Eunice Adewusi <e.adewusi@alustudent.com>
2024-09-14 22:21:41 +0200

    14. Saddle Up

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Module for performing matrix multiplication using numpy
+"""
+
+import numpy as np
+
+def np_matmul(mat1, mat2):
+    """
+    Performs matrix multiplication of two numpy arrays
+
+    Args:
+        mat1 (numpy.ndarray): The first input numpy array
+        mat2 (numpy.ndarray): The second input numpy array
+
+    Returns:
+        numpy.ndarray: The resulting numpy array after matrix multiplication
+    """
+    return np.matmul(mat1, mat2)
