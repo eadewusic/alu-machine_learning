bf0af624d841e0343ba73b6fd69ae39b4652946c
Eunice Adewusi <e.adewusi@alustudent.com>
2024-09-14 20:22:51 +0200

    1. Trim Me Down

---
+++ math/linear_algebra/1-trim_me_down.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+matrix = [[1, 3, 9, 4, 5, 8], [2, 4, 7, 3, 4, 0], [0, 3, 4, 6, 1, 5]]
+the_middle = []  # Initialise an empty list to store the 3rd and 4th columns
+for row in matrix:
+    the_middle.append([row[2], row[3]])  # Append the 3rd and 4th columns
+print("The middle columns of the matrix are: {}".format(the_middle))
