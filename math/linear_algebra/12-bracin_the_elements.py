7af8bced58a7a7306fbe017c5a4126bfa02e0950
Eunice Adewusi <e.adewusi@alustudent.com>
2024-09-14 22:08:32 +0200

    12. Bracing The Elements

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Module for performing element-wise operations on numpy arrays
+"""
+
+def np_elementwise(mat1, mat2):
+    """
+    Performs element-wise addition, subtraction, multiplication, and division.
+
+    Args:
+        mat1 (numpy.ndarray): The first input numpy array.
+        mat2 (numpy.ndarray or scalar): The second input numpy array or scalar.
+
+    Returns:
+        tuple: A tuple containing the element-wise sum, difference, product, and quotient.
+    """
+    add = mat1 + mat2
+    sub = mat1 - mat2
+    mul = mat1 * mat2
+    div = mat1 / mat2
+    return add, sub, mul, div
