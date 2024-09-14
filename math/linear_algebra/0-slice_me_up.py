b45276c4fc286f1f623226de0c5bc25555bbf727
eadewusic <e.adewusi@alustudent.com>
2024-09-14 11:09:24 -0700

    make file executable

--- math/linear_algebra/0-slice_me_up.py
+++ math/linear_algebra/0-slice_me_up.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 arr = [9, 8, 2, 3, 9, 4, 1, 0, 3]
-arr1 = arr[:2]  # First two elements (0 to index 1, excluding 2) - # Slicing the array using colon (:) operator
+arr1 = arr[:2]  # First two elements (0 to index 1, excluding 2) - Slicing the array using colon (:) operator
 arr2 = arr[-5:]  # Last five elements (from index -5 to the end)
 arr3 = arr[1:6]  # From index 1 (inclusive) to 6 (exclusive)
 print("The first two numbers of the array are: {}".format(arr1))
