aa0bc3aac03cfa38b7b30c99590eff19b810ba47
Eunice Adewusi <e.adewusi@alustudent.com>
2024-09-14 20:53:34 +0200

    pycodestyle

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -13,7 +13,8 @@ def matrix_transpose(matrix):
     Calculate the transpose of a 2D matrix.
 
     Args:
-        matrix (list of lists): A 2D list representing the matrix to be transposed.
+        matrix (list of lists): 
+        A 2D list representing the matrix to be transposed.
 
     Returns:
         list of lists: A new 2D list representing the transposed matrix.
