0bfb5a0e79816287da0406ae945e43371de5e2aa
Eunice Adewusi <e.adewusi@alustudent.com>
2024-09-14 20:50:50 +0200

    3. Flip Me Over

---
+++ math/linear_algebra/3-flip_me_over.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+"""
+3-flip_me_over - Module for transposing a 2D matrix.
+
+This module contains the matrix_transpose function which computes
+the transpose of a 2D matrix.
+"""
+
+
+def matrix_transpose(matrix):
+    """
+    Calculate the transpose of a 2D matrix.
+
+    Args:
+        matrix (list of lists): A 2D list representing the matrix to be transposed.
+
+    Returns:
+        list of lists: A new 2D list representing the transposed matrix.
+    """
+    return [list(row) for row in zip(*matrix)]
