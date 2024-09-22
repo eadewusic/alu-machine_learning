0aa6884b0c695360b1972869f961cfab03f901ae
Eunice Adewusi <e.adewusi@alustudent.com>
2024-09-22 12:34:14 +0200

    matix not symmetric bug fixed

--- math/advanced_linear_algebra/5-definiteness.py
+++ math/advanced_linear_algebra/5-definiteness.py
@@ -5,13 +5,17 @@ import numpy as np
 
 def definiteness(matrix):
     """
-    Determines the definiteness of a matrix.
-    Returns a string indicating the type of definiteness.
+    Determines the definiteness of a symmetric matrix.
+    Returns a string indicating the type of definiteness or None if not valid.
     """
     if not isinstance(matrix, np.ndarray):
         raise TypeError("matrix must be a numpy.ndarray")
 
     if matrix.ndim != 2 or matrix.shape[0] != matrix.shape[1] or matrix.size == 0:
+        return None
+
+    # Check if the matrix is symmetric
+    if not np.array_equal(matrix, matrix.T):
         return None
 
     eigenvalues = np.linalg.eigvals(matrix)
