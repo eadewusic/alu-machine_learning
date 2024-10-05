e589aee76e2b9ce68a6e873a46e74bf56c89f75a
Eunice Adewusi <e.adewusi@alustudent.com>
2024-10-05 22:50:13 +0200

    adjusted task 9

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -3,6 +3,7 @@
 Module for calculating the sum of squares of the first n integers.
 """
 
+
 def summation_i_squared(n):
     """Calculates the sum of squares of the first n integers."""
     if not isinstance(n, int) or n < 1:
