a4657881dc591fd7711e30f7974b42235093fa77
Eunice Adewusi <e.adewusi@alustudent.com>
2024-10-12 14:01:59 +0200

    3. Initialize Exponential

---
+++ math/probability/exponential.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Exponential distribution class
+"""
+
+
+class Exponential:
+    """Represents an Exponential distribution"""
+
+    def __init__(self, data=None, lambtha=1.):
+        """Initializes the Exponential distribution"""
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = float(lambtha)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.lambtha = float(1 / (sum(data) / len(data)))
