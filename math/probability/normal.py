6e593fef3bf31f422f6cbf3642299b757ad6f742
Eunice Adewusi <e.adewusi@alustudent.com>
2024-10-12 14:31:22 +0200

    8. Normal PDF

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -69,3 +69,11 @@ class Normal:
     def x_value(self, z):
         """Calculates the x-value of a given z-score"""
         return self.mean + z * self.stddev
+
+    def pdf(self, x):
+        """Calculates the value of the PDF for a given x-value"""
+        if self.stddev <= 0:
+            raise ValueError("stddev must be a positive value")
+        exponent = -((x - self.mean) ** 2) / (2 * (self.stddev ** 2))
+        return (1 / (self.stddev * (2 * 3.141592653589793) ** 0.5)) * \
+            (2.7182818285 ** exponent)
