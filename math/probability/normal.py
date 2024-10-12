c149b64c2cb3286a97808c91c775039ba005735b
Eunice Adewusi <e.adewusi@alustudent.com>
2024-10-12 14:26:14 +0200

    7. Normalize Normal

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -1,7 +1,37 @@
 #!/usr/bin/env python3
 """
-Normal distribution class
+Exponential and Normal distribution classes
 """
+
+
+class Exponential:
+    """Represents an Exponential distribution"""
+
+    def __init__(self, data=None, lambtha=1.):
+        """Initializes the Exponential distribution"""
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = float(lambtha)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.lambtha = float(1 / (sum(data) / len(data)))
+
+    def pdf(self, x):
+        """Calculates the value of the PDF for a given time period"""
+        if x < 0:
+            return 0
+        return self.lambtha * (self.lambtha ** x) * \
+            (2.7182818285 ** (-self.lambtha * x))
+
+    def cdf(self, x):
+        """Calculates the value of the CDF for a given time period"""
+        if x < 0:
+            return 0
+        return 1 - (2.7182818285 ** (-self.lambtha * x))
 
 
 class Normal:
@@ -29,5 +59,12 @@ class Normal:
     def calculate_stddev(self, data, mean):
         """Calculate the standard deviation of a list of numbers"""
         variance = sum((x - mean) ** 2 for x in data) / len(data)
-        # Standard deviation is the square root of variance
-        return variance ** 0.5
+        return variance ** 0.5  # Standard deviation is the square root of variance
+
+    def z_score(self, x):
+        """Calculates the z-score of a given x-value"""
+        return (x - self.mean) / self.stddev
+
+    def x_value(self, z):
+        """Calculates the x-value of a given z-score"""
+        return self.mean + z * self.stddev
