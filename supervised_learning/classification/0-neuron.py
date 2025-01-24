70a8cfc71a2dae51b63c69bab8e0800e74f8c096
Eunice Adewusi <e.adewusi@alustudent.com>
2025-01-24 09:03:52 +0200

    0. Neuron

---
+++ supervised_learning/classification/0-neuron.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+class Neuron:
+    """
+    Defines a single neuron performing binary classification.
+    """
+    def __init__(self, nx):
+        """
+        Class constructor.
+
+        Parameters:
+        nx (int): Number of input features to the neuron.
+
+        Raises:
+        TypeError: If nx is not an integer.
+        ValueError: If nx is less than 1.
+        """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        self.W = np.random.randn(1, nx)  # Weights vector initialized with random normal distribution
+        self.b = 0  # Bias initialized to 0
+        self.A = 0  # Activated output initialized to 0
