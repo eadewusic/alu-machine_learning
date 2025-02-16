485af37ba0bdab754883d076dc07640b054ea3fb
Eunice Adewusi <e.adewusi@alustudent.com>
2025-02-17 01:11:11 +0200

    1. Layers

---
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import tensorflow as tf
+
+
+def create_layer(prev, n, activation):
+    """
+    Creates a layer for the neural network
+
+    Args:
+    prev (tensor): The output tensor of the previous layer
+    n (int): The number of nodes in the current layer
+    activation (function): The activation function to apply to the layer
+
+    Returns:
+    tensor: The output tensor of the current layer
+    """
+    # Initialize the weights using He initialization
+    weights = tf.Variable(
+        tf.contrib.layers.variance_scaling_initializer(
+            mode="FAN_AVG")(
+            shape=[
+                prev.shape[1].value,
+                n]),
+        name='weights')
+
+    # Initialize biases as zeros
+    biases = tf.Variable(tf.zeros([n]), name='biases')
+
+    # Linear transformation (W * X + b)
+    z = tf.add(tf.matmul(prev, weights), biases, name="z")
+
+    # Apply the activation function
+    layer = activation(z, name="activation")
+
+    return layer
