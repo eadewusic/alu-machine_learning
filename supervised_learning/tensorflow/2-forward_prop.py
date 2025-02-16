e1b222e8d2a72bb6201fa1f4b9f72a0e180cc6ac
Eunice Adewusi <e.adewusi@alustudent.com>
2025-02-17 01:32:11 +0200

    2. Forward Propagation

--- supervised_learning/tensorflow/2-forward_prop.py
+++ supervised_learning/tensorflow/2-forward_prop.py
@@ -1,29 +1,44 @@
 #!/usr/bin/env python3
 """
-Defines a function that creates the forward propagation graph
-for the neural network
+This module defines the function for forward propagation in
+a neural network
+
+The function constructs the forward propagation graph by iterating
+through the layers specified in the `layer_sizes` list, applying
+the corresponding activation function from the `activations` list
+
+Functions:
+    forward_prop(x, layer_sizes=[], activations=[]): Builds the forward
+    propagation graph and returns the output tensor of the network
 """
 
-
 import tensorflow as tf
+
+# Importing the create_layer function
+create_layer = __import__('1-create_layer').create_layer
 
 
 def forward_prop(x, layer_sizes=[], activations=[]):
     """
-    Creates the forward propagation graph for the neural network
+    Creates the forward propagation graph for a neural network
 
-    parameters:
-        x [tf.placeholder]: placeholder for input data
-        layer_size [list]: contains number of nodes in each layer of network
-        activations [list]: contains activation functions for each layer
+    Args:
+    x (tensor): The placeholder for the input data
+    layer_sizes (list): A list containing the number of nodes
+    in each layer
+    activations (list): A list containing the activation functions
+    for each layer
 
-    returns:
-        prediction of the network in tensor form
+    Returns:
+    tensor: The prediction tensor of the neural network after
+    forward propagation
     """
-    create_layer = __import__('1-create_layer').create_layer
+    # Initialize the input tensor as the first layer
+    prev = x
+
+    # Loop through the layers and create them
     for i in range(len(layer_sizes)):
-        if i is 0:
-            output = create_layer(x, layer_sizes[i], activations[i])
-        else:
-            output = create_layer(output, layer_sizes[i], activations[i])
-    return output
+        prev = create_layer(prev, layer_sizes[i], activations[i])
+
+    # Return the output of the final layer
+    return prev
