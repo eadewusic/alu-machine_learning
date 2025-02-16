bb5fb92114332f608ed32794d97ff947399c51eb
Eunice Adewusi <e.adewusi@alustudent.com>
2025-02-17 01:06:11 +0200

    PEP8 issue resolved

--- supervised_learning/tensorflow/0-create_placeholders.py
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python3
+"""
+This module defines a function that creates placeholders for the input data
+and one-hot encoded labels in a neural network.
+
+The placeholders are used for feeding data into the model during training.
+
+Functions:
+    create_placeholders(nx, classes): Creates placeholders for input data and labels.
+"""
+
 import tensorflow as tf
+
 
 def create_placeholders(nx, classes):
     """Creates placeholders for input data and labels"""
     x = tf.placeholder(tf.float32, shape=(None, nx), name="x")
     y = tf.placeholder(tf.float32, shape=(None, classes), name="y")
     return x, y
+
 
 # Example usage
 if __name__ == "__main__":
