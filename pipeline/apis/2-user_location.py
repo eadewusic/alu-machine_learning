67bc7a9d66971c01d026f5b3d82de4fa71453ac7
Eunice Adewusi <e.adewusi@alustudent.com>
2025-03-17 14:01:42 +0200

    task 2

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -4,26 +4,31 @@ Script that prints the location of a GitHub user
 passed as the first argument
 """
 
-
 import requests
 from sys import argv
 from time import time
 
-
 if __name__ == "__main__":
     if len(argv) < 2:
-        raise TypeError(
-            "Input must have the full API URL passed in as an argument: {}{}".
-            format('ex. "./2-user_location.py',
-                   'https://api.github.com/users/holbertonschool"'))
+        print("Usage: ./2-user_location.py <GitHub API URL>")
+        exit(1)
+
     try:
         url = argv[1]
         results = requests.get(url)
+
+        # Handle 403 (Rate limit exceeded)
         if results.status_code == 403:
-            reset = results.headers.get('X-Ratelimit-Reset')
-            waitTime = int(reset) - time()
-            minutes = round(waitTime / 60)
+            reset = results.headers.get('X-RateLimit-Reset')
+            wait_time = int(reset) - time()
+            minutes = round(wait_time / 60)
             print('Reset in {} min'.format(minutes))
+
+        # Handle 404 (User not found)
+        elif results.status_code == 404:
+            print('Not found')
+
+        # Handle 200 (Success)
         else:
             results = results.json()
             location = results.get('location')
@@ -31,5 +36,6 @@ if __name__ == "__main__":
                 print(location)
             else:
                 print('Not found')
+
     except Exception as err:
         print('Not found')
